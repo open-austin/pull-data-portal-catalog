8226f6f0ae3f61830a04820b0ce03100a79bc37d
Chip Rosenthal <chip@unicom.com>
2013-07-10 18:39:17 -0500

    Now specify data source as a command line option. Added configurability and checking for front-end script.

--- pull-catalog.rb
+++ pull-catalog.rb
@@ -1,18 +1,64 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'net/https'
 require 'json/ext'
 require 'logger'
 
-@log = Logger.new($stderr)
-@log.level = Logger::DEBUG
+def die(args)
+  $stderr.puts(args)
+  exit(1)
+end
 
-uri = URI.parse("https://data.austintexas.gov/api/search/views.json")
+USAGE = "Usage: #{$0} [options] host_or_url (try \"--help\" for help)"
+
+$log = Logger.new($stderr)
+$log.level = Logger::INFO
+
+oparser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+  opts.separator ""
+  opts.separator "Options:"
+
+  opts.on("-q", "--quiet", "Suppress info messages") do |flag|
+    $log.level = (flag ? Logger::NOTICE : Logger::INFO)
+  end
+
+  opts.on("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+
+end
+
+begin
+  oparser.parse!
+rescue OptionParser::InvalidOption => e
+  die "#{$0}: #{e} (try \"--help\" for help)"
+end
+
+die USAGE unless ARGV.length == 1
+arg = ARGV.shift
+if arg =~ /^http/
+  url = arg
+else
+  url = "https://#{arg}/api/search/views.json"
+end
+uri = URI.parse(url)
 
 http = Net::HTTP.new(uri.host, uri.port)
 if uri.scheme == "https"
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+end
+
+def http.perform_get(uri)
+  $log.info {">>> #{uri}"}
+  req = Net::HTTP::Get.new(uri.request_uri)
+  res = self.request(req)
+  $log.info {"<<< HTTP #{res.code} #{res.message} (size = #{res.body.length})"}
+  raise "HTTP get failed [#{res.code} #{res.message}]" unless res.is_a?(Net::HTTPSuccess)
+  res
 end
 
 data = nil
@@ -25,11 +71,7 @@ while max_datasets.nil? || num_datasets < max_datasets
   params = {:limitTo => 'TABLES', :limit => 100, :page => page}
   uri.query = URI.encode_www_form(params)
 
-  @log.debug {">>> #{uri}"}
-  request = Net::HTTP::Get.new(uri.request_uri)
-  response = http.request(request)
-  @log.debug {"<<< HTTP #{response.code} #{response.message} (size = #{response.body.length})"}
-  raise "HTTP get failed [#{response.code} #{response.message}]" unless response.is_a?(Net::HTTPSuccess)
+  response = http.perform_get(uri)
 
   a = JSON.parse(response.body)
   if data.nil?
@@ -45,5 +87,5 @@ while max_datasets.nil? || num_datasets < max_datasets
 end
 
 puts data.to_json
-@log.debug {"dumped metadata for #{data['results'].length} datasets"}
+$log.info {"dumped metadata for #{data['results'].length} datasets"}
 
--- run-pull-catalog.sh
+++ run-pull-catalog.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
 PULL_SCRIPT="`dirname $0`/pull-catalog.rb"
-SAVE_FILE="/srv/www/data.open-austin.org/html/data-catalogs/`date +'catalog-data_austintexas_gov-%Y%m%d.json'`"
+
+HOST="data.austintexas.gov"
+SAVE_DIR="/srv/www/data.open-austin.org/html/data-catalogs"
+
+DATESTAMP=`date +'%Y%m%d'`
+HOSTMUNGED=`echo "$HOST" | sed -e 's/\./_/g'`
+SAVE_FILE="${SAVE_DIR}/catalog-${HOSTMUNGED}-${DATESTAMP}.json"
 TEMP_FILE="$SAVE_FILE.$$"
 
-$PULL_SCRIPT > $TEMP_FILE
+if [ ! -d "$SAVE_DIR" ] ; then
+	echo "$0: destination directory does not exist: $SAVE_DIR" >&2
+	exit 1
+fi
+if [ -f "$SAVE_FILE" ] ; then
+	echo "$0: will not overwrite destination: $SAVE_FILE" >&2
+	exit 1
+fi
+
+$PULL_SCRIPT $HOST > $TEMP_FILE
 if [ $? -ne 0 ] ; then
 	echo "$0: pull failed - output saved to: $TEMP_FILE" >&2
 	exit 1
