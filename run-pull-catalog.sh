44690b6d440aee0291323182911522fe39febb66
Chip Rosenthal <chip@unicom.com>
2013-07-10 20:08:16 -0500

    New summarize-catalog.rb script produces easy-to-digest summarized versions of the catalog metadata. In nightly script, generate summary after fetching data.

--- run-pull-catalog.sh
+++ run-pull-catalog.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-PULL_SCRIPT="`dirname $0`/pull-catalog.rb"
+PATH="`dirname $0`:$PATH"
+export PATH
+
+PULL_SCRIPT="pull-catalog.rb"
+SUMMARIZE_SCRIPT="summarize-catalog.rb"
 
 HOST="data.austintexas.gov"
-SAVE_DIR="/srv/www/data.open-austin.org/html/data-catalogs"
+#SAVE_DIR="/srv/www/data.open-austin.org/html/data-catalogs"
+SAVE_DIR="/tmp"
 
 DATESTAMP=`date +'%Y%m%d'`
 HOSTMUNGED=`echo "$HOST" | sed -e 's/\./_/g'`
 SAVE_FILE="${SAVE_DIR}/catalog-${HOSTMUNGED}-${DATESTAMP}.json"
 TEMP_FILE="$SAVE_FILE.$$"
+SUMMARY_FILE="${SAVE_DIR}/summary-${HOSTMUNGED}-${DATESTAMP}.json"
 
 if [ ! -d "$SAVE_DIR" ] ; then
 	echo "$0: destination directory does not exist: $SAVE_DIR" >&2
@@ -26,6 +32,10 @@ if [ $? -ne 0 ] ; then
 fi
 
 mv $TEMP_FILE $SAVE_FILE
-echo "$0: pull complete - output saved to: $SAVE_FILE" >&2
+echo "$0: metadata saved to: $SAVE_FILE" >&2
+
+$SUMMARIZE_SCRIPT $SAVE_FILE >$SUMMARY_FILE
+echo "$0: summary saved to: $SUMMARY_FILE" >&2
+
 exit 0
 
---
+++ summarize-catalog.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+
+require 'json/ext'
+
+USAGE = "Usage: #{$0} metadata.json"
+
+def die(args)
+  $stderr.puts(args)
+  exit(1)
+end
+
+die USAGE unless ARGV.length == 1
+filename = ARGV.shift
+
+# Slurp in the full catalog.
+data = open(filename) {|fp| JSON.parse(fp.read)}
+
+# Build a summarized list of datsets from data['results']
+datasets = []
+
+data['results'].each do |dataset|
+  a = dataset['view']
+
+  #
+  # The "metadata" object is mostly boring, but the City of Austin
+  # uses the "custom_fields" to indicate things such as
+  # departments and update frequency.
+  #
+  a['customFields'] = a['metadata']['custom_fields']
+
+  #
+  # Believe it or not, Socrata does not provide any information on
+  # the dataset size in the metadata. It does, however, provide
+  # a detailed listing of the columns, including the count of non-null
+  # values for the column. We'll pick the largest such count to be
+  # the dataset row count.
+  #
+  a['rowCount'] = a['columns'].map do |b|
+    b.has_key?('cachedContents') ? b['cachedContents']['non_null'] : nil
+  end.max
+
+  #
+  # The following elements are nested objects and arrays that probably
+  # aren't going to be useful for analysis. So, remove them.
+  #
+  %w(
+    columns
+    flags
+    grants
+    metadata
+    owner
+    query
+    rights
+    sortBys
+    tableAuthor
+    viewFilters
+  ).each {|key| a.delete(key)}
+
+  datasets << a
+end
+
+# Drop the detailed metadata and add the summary list.
+data.delete('results')
+data['datasets'] = datasets
+
+puts data.to_json
+
